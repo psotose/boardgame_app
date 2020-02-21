8c3bf376a16f6e22fa4f159fb4f17135a78567fd
patito <p.sotosellanes@hotmail.com>
2020-02-21 10:17:32 -0300

    sizing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :boardgames do
-    delete 'delete_image/:image_id', on: :member, to: 'boardgames#delete_image',
-    as: 'delete_image'
+    delete 'delete_image/:image_id', on: :member, to: 'boardgames#delete_image', as: 'delete_image'
   end
   root 'boardgames#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
